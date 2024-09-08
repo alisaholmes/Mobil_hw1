eabd6449865cd7eedfd6819ba82bb5d27944062f
Julia Isaacson <alisa.holmes@mail.ru>
2024-09-08 19:52:51 +0300

    first commit

---
+++ test_android_app/conftest.py
@@ -0,0 +1,56 @@
+import allure
+import pytest
+import allure_commons
+from appium.options.android import UiAutomator2Options
+from selene import browser, support
+import os
+from dotenv import load_dotenv
+from appium import webdriver
+from utils import attach
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function', autouse=True)
+def mobile_management():
+    login = os.getenv('LOGIN')
+    access_key = os.getenv('PASSWORD')
+    options = UiAutomator2Options().load_capabilities({
+        # Specify device and os_version for testing
+        # "platformName": "android"
+        'platformVersion': '9.0',
+        'deviceName': 'Google Pixel 3',
+        # Set URL of the application under test
+        'app': os.getenv('APP_URL'),
+
+        # Set other BrowserStack capabilities
+        'bstack:options': {
+            'projectName': 'First Python project',
+            'buildName': 'browserstack-build-1',
+            'sessionName': 'BStack first_test',
+        # Set your access credentials
+            'userName': login,
+            'accessKey': access_key
+        }
+    })
+
+    with allure.step('Инициализировать сеанс приложения'):
+        browser.config.driver_remote_url = 'http://hub.browserstack.com/wd/hub'
+        browser.config.driver_options = options
+
+        browser.config.timeout = float(os.getenv('timeout', '10.0'))
+        browser.config._wait_decorator = support._logging.wait_with(context=allure_commons._allure.StepContext)
+
+    yield
+
+    attach.add_screenshot(browser)
+    attach.add_xml(browser)
+
+    session_id = browser.driver.session_id
+
+    attach.add_video(session_id, login, access_key)
+
+    with allure.step('Закрыть сессию приложения'):
+        browser.quit()
---
+++ test_android_app/test_wiki_app.py
@@ -0,0 +1,36 @@
+import allure
+from allure_commons.types import Severity
+from appium.webdriver.common.appiumby import AppiumBy
+from selene import browser, have
+
+
+@allure.tag('Mobile')
+@allure.severity(Severity.NORMAL)
+@allure.label('owner', 'Alisa Holmes')
+@allure.feature('Найти статью о «Appium»')
+@allure.story('Поиск статьи')
+def test_search():
+    with allure.step('Ввести в поиск значение "Appium"'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID,'org.wikipedia.alpha:id/search_src_text')).type('Appium')
+    with allure.step('Проверить найденное значение'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('Appium'))
+
+
+@allure.tag('Mobile')
+@allure.severity(Severity.NORMAL)
+@allure.label('owner', 'Alisa Holmes')
+@allure.feature('Найти статью о "HTML"')
+@allure.story('Поиск статьи')
+def test_search_HTML():
+    with allure.step('Ввести в поиск "HTML"'):
+        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
+        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('HTML')
+    with allure.step('Проверить найденное значение'):
+        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
+        results.should(have.size_greater_than(0))
+        results.first.should(have.text('HTML'))
+    with allure.step('Клик на первый результат'):
+        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
