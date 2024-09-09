1c6dc06425f51056812ecf824eea13fc8edbfa98
Julia Isaacson <alisa.holmes@mail.ru>
2024-09-09 12:20:37 +0300

    правки

--- test_android_app/test_wiki_app.py
+++ test_android_app/test_wiki_app.py
@@ -25,9 +25,9 @@ def test_search():
 @allure.feature('Найти статью о "HTML"')
 @allure.story('Поиск статьи')
 def test_search_html():
-    with allure.step('Ввести в поиск "HTML"'):
+    with allure.step('Ввести в поиск значение "HTML"'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/search_src_text')).type('HTML')
+        browser.element((AppiumBy.ID,'org.wikipedia.alpha:id/search_src_text')).type('HTML')
     with allure.step('Проверить найденное значение'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
