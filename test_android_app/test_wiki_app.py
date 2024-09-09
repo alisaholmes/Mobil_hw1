f61a34cd011687e9c4b8647dc676d8468b97bcd0
Julia Isaacson <alisa.holmes@mail.ru>
2024-09-09 15:18:24 +0300

    .

--- test_android_app/test_wiki_app.py
+++ test_android_app/test_wiki_app.py
@@ -22,15 +22,15 @@ def test_search():
 @allure.tag('Mobile')
 @allure.severity(Severity.NORMAL)
 @allure.label('owner', 'Alisa Holmes')
-@allure.feature('Найти статью о "Java"')
+@allure.feature('Найти статью о "HTML"')
 @allure.story('Поиск статьи')
-def test_search_java():
-    with allure.step('Ввести в поиск значение "Java"'):
+def test_search_html():
+    with allure.step('Ввести в поиск значение "HTML"'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Java')
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('HTML')
     with allure.step('Проверить найденное значение'):
         results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
         results.should(have.size_greater_than(0))
-        results.first.should(have.text('Java'))
-   # with allure.step('Клик на первый результат'):
-       # browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
+        results.first.should(have.text('HTML'))
+    with allure.step('Клик на первый результат'):
+        browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')).first.click()
